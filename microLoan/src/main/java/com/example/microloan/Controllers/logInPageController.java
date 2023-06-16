d14b27623fa97c4a531a56d06de078708af3f1bf
cristi <cristilive6@gmail.com>
2023-06-16 12:09:34 -0700

    testing if git is accessible

---
+++ microLoan/src/main/java/com/example/microloan/Controllers/logInPageController.java
@@ -0,0 +1,16 @@
+package com.example.microloan.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RequestMapping
+@Controller
+public class logInPageController {
+
+
+
+
+
+}
+
