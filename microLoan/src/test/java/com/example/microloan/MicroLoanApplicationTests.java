cb50e10bc0e2a56ec41711d1d67511a0b8a0b0c2
jackjack272 <69359942+jackjack272@users.noreply.github.com>
2023-06-16 11:58:18 -0700

    Add files via upload
    this is the inital blank file

---
+++ microLoan/src/test/java/com/example/microloan/MicroLoanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microloan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroLoanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
